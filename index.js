d006646d7c0b4a91e3a791a988e47a119e55ac3a
Way <maynv.uet@gmail.com>
2018-04-05 10:37:37 +0700

    change

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ io.sockets.on('connection', function(socket) {
   // convenience function to log server messages on the client
   function log() {
     var array = ['Message from server:'];
-    array.push.apply(array, arguments);
-    socket.emit('log', array);
+    for (var i = 0; i < arguments.length; i++) {
+        array.push(arguments[i]);
+      }
+        socket.emit('log', array);
   }
 
   socket.on('message', function(message) {
@@ -55,7 +57,7 @@ io.sockets.on('connection', function(socket) {
     var ifaces = os.networkInterfaces();
     for (var dev in ifaces) {
       ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+        if (details.family === 'IPv4' && details.address !== '10.90.136.27') {
           socket.emit('ipaddr', details.address);
         }
       });
